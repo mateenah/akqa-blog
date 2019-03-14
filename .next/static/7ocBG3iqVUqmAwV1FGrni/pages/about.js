d900f0ca66854a400302149aa5b1e321e1276fec
Snehil Bhushan <Snehil.Bhushan@akqa.com>
2019-03-14 15:53:34 +0100

    removed .next

--- .next/static/7ocBG3iqVUqmAwV1FGrni/pages/about.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{Juyh:function(n,o){},rB5V:function(n,o,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about",function(){var n=u("Juyh");return{page:n.default||n}}])}},[["rB5V",1]]]);
